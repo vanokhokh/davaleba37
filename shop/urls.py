ca1a4c9567f5fd214c10a6dc2c586c7ee3d93cd1
vanokhokh <vanoxoxobashvili429@gmail.com>
2025-09-25 15:55:32 +0400

    davaleba38_shevcvale kerdzod urls.py radgan ar gadadioda konkretul productze

--- shop/urls.py
+++ shop/urls.py
@@ -5,8 +5,8 @@ app_name='shop'
 
 urlpatterns = [
     path('', index, name='home'),
-    path('<int:category_pk>/', specific_categories, name='specific_categories'),
-    path('<int:product_pk>/', product, name='product'),
+    path('specific_category/<int:category_pk>/', specific_categories, name='specific_categories'),
+    path('product/<int:product_pk>/', product, name='product'),
     path('discounted/', discounted_product, name='discounted_product'),
     path('all_products/', all_products, name='all_products'),
     path('add_product/', add_product, name='add_product'),
